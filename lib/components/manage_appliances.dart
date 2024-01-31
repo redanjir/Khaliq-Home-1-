4ab449187a495e033454c28787959f194935f3ec
redanjir <glock68bedok@gmail.com>
2024-01-31 09:38:16 +0800

    Finished the manage_appliances_page

--- lib/components/manage_appliances.dart
+++ lib/components/manage_appliances.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+
 import 'package:flutter/material.dart';
+import 'package:flutter_apptest/pages/manage_appliances2.dart';
 
 class ManageAppliances extends StatefulWidget {
   final String title;
@@ -12,33 +15,38 @@ class ManageAppliances extends StatefulWidget {
 class _ManageAppliancesState extends State<ManageAppliances> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Theme.of(context).colorScheme.primary
-        ),
-        width: 380,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(
-              height: 300, // Image size basically
-              child: Image(image: AssetImage(widget.imagepath)),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(widget.title, style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
-                ],
+    return GestureDetector(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Theme.of(context).colorScheme.primary
+          ),
+          width: 380,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 300, // Image size basically
+                child: Image(image: AssetImage(widget.imagepath)),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(widget.title, style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) => ManageAppliancesNextPage(widget.title, widget.imagepath)));
+      },
     );
   }
 }
