ec1998cdb10f05a1be3c70ae25711cb5fdda5ab1
redanjir <glock68bedok@gmail.com>
2024-02-06 13:26:19 +0800

    a

--- lib/pages/manage_appliances/manageFans.dart
+++ lib/pages/manage_appliances/manageFans.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_apptest/models/fans.dart';
 import 'package:flutter_apptest/pages/appliances_settings/fan_settings_page.dart';
